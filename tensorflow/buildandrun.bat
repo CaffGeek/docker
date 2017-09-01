6dd5a42bf839de050fe81a7e8520422335c90b71
Chad Hurd <gibble@gmail.com>
2017-09-01 15:27:13 -0500

    back to port 5000

--- tensorflow/buildandrun.bat
+++ tensorflow/buildandrun.bat
@@ -1,3 +1,3 @@
 cls
 docker build . -t caffgeek/tensorflow
-docker run --rm -it -p 5000:80 --name tensorflow caffgeek/tensorflow
+docker run --rm -it -p 5000:5000 --name tensorflow caffgeek/tensorflow
