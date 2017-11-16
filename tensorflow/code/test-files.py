7c2222c5270fb1833522e0e3d2f5f46c4f68c236
Chad Hurd <gibble@gmail.com>
2017-11-16 16:36:37 -0600

    don't need old test-files.py

--- tensorflow/code/test-files.py
+++
@@ -1,7 +0,0 @@
-import label_image
-
-labels = label_image.load_labels('/tf/tf_files/retrained_labels.txt')
-label_image.load_graph('/tf/tf_files/retrained_graph.pb')
-
-image_data = label_image.load_image('/images/bowling/11111.1.jpg')
-label_image.run_graph(image_data, labels, 'DecodeJpeg/contents:0', 'final_result:0', 5)
