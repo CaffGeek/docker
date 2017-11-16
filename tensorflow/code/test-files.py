6c40ed81cb9168ed30f203d4c1666c0ca150fc8c
Chad Hurd <gibble@gmail.com>
2017-11-16 14:24:43 -0600

    reorg of code

--- tensorflow/code/test-files.py
+++ tensorflow/code/test-files.py
@@ -1,7 +1,7 @@
 import label_image
 
-image_data = label_image.load_image('/images/bowling/11111.1.jpg')
-labels = label_image.load_labels('tf_files/retrained_labels.txt')
-label_image.load_graph('tf_files/retrained_graph.pb')
+labels = label_image.load_labels('/tf/tf_files/retrained_labels.txt')
+label_image.load_graph('/tf/tf_files/retrained_graph.pb')
 
+image_data = label_image.load_image('/images/bowling/11111.1.jpg')
 label_image.run_graph(image_data, labels, 'DecodeJpeg/contents:0', 'final_result:0', 5)
